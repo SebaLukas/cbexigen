19672ec078300e27cf974bc5c281ef3fdb090039
Sebastian Lukas <sisasebbl@googlemail.com>
2023-06-05 17:50:02 +0200

    Adding encoder_u8, u16, u32 & u64()
    Signed-off-by: Sebastian Lukas <sisasebbl@googlemail.com>

--- rustv2g/src/common/exi_basetypes_encoder.rs
+++ rustv2g/src/common/exi_basetypes_encoder.rs
@@ -2,6 +2,13 @@ use crate::common::exi_error_codes::ExiError;
 use crate::common::exi_bitstream::ExiBitstream;
 use crate::common::exi_basetypes::ExiUnsigned;
 
+use crate::common::exi_basetypes::{
+    EXI_BASETYPES_MAX_OCTETS_SUPPORTED,
+    EXI_BASETYPES_UINT8_MAX_OCTETS,
+    EXI_BASETYPES_UINT16_MAX_OCTETS,
+    EXI_BASETYPES_UINT32_MAX_OCTETS,
+};
+
 fn write_unsigned(stream: &mut ExiBitstream, exi_unsigned: &ExiUnsigned) -> Result<(), ExiError> {
 
     for n in 0..*exi_unsigned.octets_count() {
@@ -39,6 +46,37 @@ pub fn encoder_nbit_uint(stream: &mut ExiBitstream, bit_count: usize, value: u32
     stream.write_bits(bit_count, value)
 }
 
+pub fn encoder_u8(stream: &mut ExiBitstream, value: u8) -> Result<(), ExiError> {
+
+    let mut exi_unsigned = ExiUnsigned::new([0; EXI_BASETYPES_MAX_OCTETS_SUPPORTED], 0);
+    exi_unsigned.convert32_to(value as u32, EXI_BASETYPES_UINT8_MAX_OCTETS)?;
+
+    write_unsigned(stream, &mut exi_unsigned)
+}
+
+pub fn encoder_u16(stream: &mut ExiBitstream, value: u16) -> Result<(), ExiError> {
+
+    let mut exi_unsigned = ExiUnsigned::new([0; EXI_BASETYPES_MAX_OCTETS_SUPPORTED], 0);
+    exi_unsigned.convert32_to(value as u32, EXI_BASETYPES_UINT16_MAX_OCTETS)?;
+
+    write_unsigned(stream, &mut exi_unsigned)
+}
+
+pub fn encoder_u32(stream: &mut ExiBitstream, value: u32) -> Result<(), ExiError> {
+
+    let mut exi_unsigned = ExiUnsigned::new([0; EXI_BASETYPES_MAX_OCTETS_SUPPORTED], 0);
+    exi_unsigned.convert32_to(value, EXI_BASETYPES_UINT32_MAX_OCTETS)?;
+
+    write_unsigned(stream, &mut exi_unsigned)
+}
+
+pub fn encoder_u64(stream: &mut ExiBitstream, value: u64) -> Result<(), ExiError> {
+
+    let mut exi_unsigned = ExiUnsigned::new([0; EXI_BASETYPES_MAX_OCTETS_SUPPORTED], 0);
+    exi_unsigned.convert64_to(value)?;
+
+    write_unsigned(stream, &mut exi_unsigned)
+}
 
 mod tests {
     use super::*;
@@ -92,6 +130,60 @@ mod tests {
             expected.push(exi_stream.read_octet().unwrap())
         }
         assert_eq!(expected, bytes);
+    }
+
+    #[test]
+    fn test_encoder_u8() {
+        let vector = vec![0; 1024];
+        let vector_len = vector.len();
+        let mut exi_stream = ExiBitstream::new(vector, vector_len, 0);
+        
+        assert_eq!(encoder_u8(&mut exi_stream, 0x56).is_ok(), true);
+
+        exi_stream.reset();
+        assert_eq!(exi_stream.read_octet(), Ok(0x56));
+    }
+
+    #[test]
+    fn test_encoder_u16() {
+        let vector = vec![0; 1024];
+        let vector_len = vector.len();
+        let mut exi_stream = ExiBitstream::new(vector, vector_len, 0);
+        
+        assert_eq!(encoder_u16(&mut exi_stream, 0x5678).is_ok(), true);
+
+        exi_stream.reset();
+        assert_eq!(exi_stream.read_octet(), Ok(0b11111000));
+        assert_eq!(exi_stream.read_octet(), Ok(0b10101100));
+        assert_eq!(exi_stream.read_octet(), Ok(0b00000001));
+    }
+
+    #[test]
+    fn test_encoder_u32() {
+        let vector = vec![0; 1024];
+        let vector_len = vector.len();
+        let mut exi_stream = ExiBitstream::new(vector, vector_len, 0);
+        
+        assert_eq!(encoder_u32(&mut exi_stream, 0x5678).is_ok(), true);
+
+        exi_stream.reset();
+        assert_eq!(exi_stream.read_octet(), Ok(0b11111000));
+        assert_eq!(exi_stream.read_octet(), Ok(0b10101100));
+        assert_eq!(exi_stream.read_octet(), Ok(0b00000001));
+    }
+
+    #[test]
+    fn test_encoder_u64() {
+        let vector = vec![0; 1024];
+        let vector_len = vector.len();
+        let mut exi_stream = ExiBitstream::new(vector, vector_len, 0);
+        
+        assert_eq!(encoder_u64(&mut exi_stream, 0x5678).is_ok(), true);
+
+        exi_stream.reset();
+        assert_eq!(exi_stream.read_octet(), Ok(0b11111000));
+        assert_eq!(exi_stream.read_octet(), Ok(0b10101100));
+        assert_eq!(exi_stream.read_octet(), Ok(0b00000001));
     }
 
 }
